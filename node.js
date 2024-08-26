ddf2b7158b58b6686da070b1a047fc757bfa6263
lfmutton <lfmutton@gmail.com>
2024-08-26 19:37:54 -0300

    first commit

---
+++ node.js
@@ -0,0 +1,53 @@
+class Grid {
+    constructor(matrix) {
+        this.matrix = matrix;
+        this.uiContext = this.DrawGrid(600, 900)
+    }
+    DrawGrid(w, h, color = "#1111") {
+        this.canvas = document.querySelector("canvas")
+        this.context = this.canvas.getContext("2d")
+        this.width = this.canvas.height = w
+        this.height = this.canvas.width = h
+        this.canvas.style.position = "absolute"
+        this.canvas.style.background = color
+        this.canvas.style.marginLeft = window.innerWidth / 2 - w / 2 + "px"
+        this.canvas.style.marginTop = window.innerHeight / 2 - h / 2 + "px"
+        document.body.appendChild(this.canvas)
+
+        return this.context
+    }
+}
+
+class Square { }
+class NormalL { }
+class InvertedL { }
+class NormalZ { }
+class InvertedZ { }
+class Line { }
+class LastPiece { }
+
+const matrix = [
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+]
+
+const gridSystem = new Grid(matrix)
